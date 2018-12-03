c788421319c79ec5596bed84a1fb4cfd2133b44a
Austin Odell <austin.odell@duke.edu>
2018-12-02 21:52:39 -0500

    Working UF

--- src/PercolationUF.java
+++ src/PercolationUF.java
@@ -1,34 +1,85 @@
 
 public class PercolationUF implements IPercolate{
-
+	
+	private boolean[][] myGrid;
+	int myOpenCount = 0;
+	int mySize;
+	IUnionFind myFinder = new QuickUWPC();
+	int VTOP;
+	int VBOTTOM;
+	
+	public PercolationUF(int size, IUnionFind finder) {
+		mySize = size;
+		VTOP = size*size;
+		VBOTTOM = VTOP + 1;
+		myGrid = new boolean[size][size];
+		myFinder = finder;
+		myFinder.initialize(size*size + 2);
+	}
+	
 	@Override
 	public void open(int row, int col) {
 		// TODO Auto-generated method stub
-		
+		if (! inBounds(row,col)) {
+			throw new IndexOutOfBoundsException(
+					String.format("(%d,%d) not in bounds", row,col));
+		}
+		if (myGrid[row][col] != true) {
+			myGrid[row][col] = true; 
+			myOpenCount ++;
+			if (row == 0)
+				myFinder.union(col, VTOP);
+			if (row == mySize-1)
+				myFinder.union(mySize * row + col, VBOTTOM);
+			if (inBounds(row + 1, col) && myGrid[row + 1][col])
+				myFinder.union(mySize * row + col, mySize * (row + 1) + col);
+			if (inBounds(row - 1, col) && myGrid[row - 1][col])
+				myFinder.union(mySize * row + col, mySize * (row - 1) + col);
+			if (inBounds(row , col + 1) && myGrid[row][col + 1])
+				myFinder.union(mySize * row + col, mySize * row + 1 + col);
+			if (inBounds(row , col - 1) && myGrid[row][col - 1])
+				myFinder.union(mySize * row + col, mySize * row - 1 + col);
+		}
+
+	}
+
+	private boolean inBounds(int row, int col) {
+		// TODO Auto-generated method stub
+		if (row >= mySize  || col >= mySize  || row < 0 || col < 0)
+			return false;
+		return true;
 	}
 
 	@Override
 	public boolean isOpen(int row, int col) {
-		// TODO Auto-generated method stub
-		return false;
+		if (! inBounds(row,col)) {
+			throw new IndexOutOfBoundsException(
+					String.format("(%d,%d) not in bounds", row,col));
+		}
+		return myGrid[row][col];
 	}
+	
 
 	@Override
 	public boolean isFull(int row, int col) {
 		// TODO Auto-generated method stub
-		return false;
+		if (! inBounds(row,col)) {
+			throw new IndexOutOfBoundsException(
+					String.format("(%d,%d) not in bounds", row,col));
+		}
+		return myFinder.connected(VTOP, row* mySize + col);
 	}
 
 	@Override
 	public boolean percolates() {
 		// TODO Auto-generated method stub
-		return false;
+		return myFinder.connected(VTOP, VBOTTOM);
 	}
 
 	@Override
 	public int numberOfOpenSites() {
 		// TODO Auto-generated method stub
-		return 0;
+		return myOpenCount;
 	}
 
 }
